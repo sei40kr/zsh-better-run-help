80eaec1577f46d25024b88633ab27d93d4829421
Seong Yong-ju <sei40kr@gmail.com>
2018-03-14 02:25:56 +0900

    Add files

---
+++ zsh-better-run-help.plugin.zsh
@@ -0,0 +1,39 @@
+_man_page_exists() {
+  local page="$1"
+  man -w "$page" 1>/dev/null 2>/dev/null
+}
+
+better-run-help() {
+  local args=( ${=BUFFER} )
+  local command="${args[1]}"
+  local subcommand="${args[2]}"
+
+  if [[ "$command" == "" || "$command" == "man" || "$command" == "run-help" ]]
+  then
+    # Do nothing
+    return
+  fi
+
+  if [[ "$subcommand" != "" ]] && _man_page_exists "${command}-${subcommand}"
+  then
+    LBUFFER="man ${command}-${subcommand}"
+    zle accept-line
+    zle -R -c
+    return
+  fi
+
+  if _man_page_exists "$command"; then
+    LBUFFER="man ${command}"
+    zle accept-line
+    zle -R -c
+    return
+  fi
+
+  if [[ "${+commands[$command]}" == 1 ]]; then
+    LBUFFER="${command} ${subcommand} --help | \$PAGER"
+  fi
+}
+
+zle -N better-run-help
+
+bindkey "^[h" better-run-help
